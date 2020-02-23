f210e5e7b8f89dfe30cd2c8c19095921afa18a97
Sommer Olson <olsozom@gmail.com>
2020-02-22 19:16:07 -0600

    Fixing more layout issues

--- app/src/main/java/com/example/lab4milestone2/MainActivity.java
+++ app/src/main/java/com/example/lab4milestone2/MainActivity.java
@@ -61,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
 
         if (Build.VERSION.SDK_INT >= 23 &&
                 ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
             ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         }
         else {
@@ -70,8 +71,6 @@ public class MainActivity extends AppCompatActivity {
                 updateLocationInfo(location);
             }
         }
-
-
     }
 
     public void startListening() {
@@ -115,6 +114,7 @@ public class MainActivity extends AppCompatActivity {
             List<Address> listAddresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
 
             if (listAddresses != null && listAddresses.size() > 0) {
+
                 Log.i("PlaceInfo", listAddresses.get(0).toString());
                 address = "Address: \n";
 
